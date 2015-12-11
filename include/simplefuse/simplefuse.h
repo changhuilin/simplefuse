6cdeb2edbe9ee6aef8c588fc35598499e5a933d8
Changhui Lin <changhui.lin@acm.org>
2015-12-11 00:20:51 -0800

    interface

--- include/simplefuse/simplefuse.h
+++ include/simplefuse/simplefuse.h
@@ -2,6 +2,21 @@
 #define __SIMPLEFUSE_H__
 
 
-
-
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+int sfuse_open(const char *pathname, int flags);
+
+int sfuse_close(int fd);
+
+ssize_t sfuse_read(int fd, void *buf, size_t count);
+
+ssize_t sfuse_write(int fd, const void *buf, size_t count);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //__SIMPLEFUSE_H__
